306be3cd4e6726b4243593828906300909522519
153918928+oshanavishkapiries@users.noreply.github.com <153918928+oshanavishkapiries@users.noreply.github.com>
2025-08-22 14:12:07 +0530

    feat (image) : cdn-image-link-change

--- data/images.ts
+++ data/images.ts
@@ -1,7 +1,7 @@
 export const IMAGES = {
     LOGO:"https://res.cloudinary.com/comseeker/image/upload/v1747459447/cenzios/logo_i7vu6i.png",
-    LOGO_B:"https://res.cloudinary.com/comseeker/image/upload/v1747459447/cenzios/logo-b_palese.png",
-    LOGO_W:"https://res.cloudinary.com/comseeker/image/upload/v1747459448/cenzios/logo-w_adv9kh.png",
+    LOGO_B:"https://res.cloudinary.com/comseeker/image/upload/v1755851952/cenzios/logo-b_hv4z1k.png",
+    LOGO_W:"https://res.cloudinary.com/comseeker/image/upload/v1755851952/cenzios/logo-w_scpfij.png",
     ABOUT_ANIMATION:"https://res.cloudinary.com/comseeker/raw/upload/v1747459449/cenzios/aboutAnimation_v3gc3t.json",
     HERO_ANIMATION:"https://res.cloudinary.com/comseeker/raw/upload/v1747459449/cenzios/hero_mrzqmo.json",
     MOBILE_DEV_ANIMATION:"https://res.cloudinary.com/comseeker/raw/upload/v1747459449/cenzios/mobile-dev_zqoj6o.json",
